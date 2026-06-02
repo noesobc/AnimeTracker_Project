55a2e5614910067220ccd51ce1c1e4ba2cb2d4a2
Noelia <132777122+noesobc@users.noreply.github.com>
2026-06-02 13:54:38 +0300

    Add files via upload

---
+++ src/services/animeService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/animes";
+
+export const getAllAnimes = () => {
+  return axios.get(API_URL);
+};
+
+export const createAnime = (anime) => {
+  return axios.post(API_URL, anime);
+};
+
+export const updateAnime = (id, anime) => {
+  return axios.put(`${API_URL}/${id}`, anime);
+};
+
+export const deleteAnime = (id) => {
+  return axios.delete(`${API_URL}/${id}`);
+};
